2188db52e4d3a3882f0a91df2c580a4889e9ed3d
netyouli <712641411@qq.com>
2016-07-01 11:27:38 +0800

    Initial Commit

---
+++ WHC_ScanUnreferenceImageTool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WHC_ScanUnreferenceImageTool
+//
+//  Created by WHC on 16/7/1.
+//  Copyright © 2016年 WHC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WHC_ScanUnreferenceImageTool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WHC_ScanUnreferenceImageTool
+//
+//  Created by WHC on 16/7/1.
+//  Copyright © 2016年 WHC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
